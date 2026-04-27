52410a74b7d91777134d91bb50badf7e21ca6a4d
Francisco Javier Salido Machado <javier87645@msn.com>
2026-04-27 12:41:17 +0200

    Tarea 4

--- app/src/main/java/dam/pmdm/spyrothedragon/models/Collectible.kt
+++
@@ -1,7 +0,0 @@
-package dam.pmdm.spyrothedragon.models
-
-data class Collectible(
-    var name: String? = null,
-    var description: String? = null,
-    var image: String? = null
-)
--- app/src/main/java/dam/pmdm/spyrothedragon/models/World.kt
+++
@@ -1,7 +0,0 @@
-package dam.pmdm.spyrothedragon.models
-
-data class World(
-    var name: String? = null,
-    var description: String? = null,
-    var image: String? = null
-)
